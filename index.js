bc841b2a44c44030437d495ab038553cfe2d4a24
Denissecs <131845862+Denissecs@users.noreply.github.com>
2023-11-15 10:49:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const matriz = [[0, 0, 0, 0, 0],
-[0, 1, 1, 1, 0],
-[0, 1, 0, 1, 0],
-[0, 1, 1, 1, 0],
-[0, 0, 0, 0, 0]];
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+const matriz = [[0, 0, 0, 0, 0],        
+                [0, 1, 1, 0, 0],
+                [0, 1, 0, 1, 0],
+                [0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0]];
+
 
 const vecinos = (m) => {
+    
     const fila = m.length;//largo de la fila
-    const columna = m[0].length;//largo de la columna    
+    const columna = m[0].length;//largo de la columna  
+    
     const contarvecinos = (r, c) => {
         let contar = 0;
         for (let x = -1; x <= 1; x++) {
@@ -19,8 +24,7 @@ const vecinos = (m) => {
             }
         }
         return contar;
-    };
-    console.log(m)
+    }
     for (let i = 1; i < fila; i++) {
         for (let j = 1; j < columna; j++) {
             const cell = m[i][j];
@@ -28,41 +32,38 @@ const vecinos = (m) => {
 
             if (cell === 0 && nv === 3) {
                 m[i][j] = 1;
-                console.log(m)
+        
             } else if (cell === 1 && (nv < 2 || nv > 3)) {
                 m[i][j] = 0;
-                console.log(m)
+               
+    
             }
         }
     }
+
 };
-vecinos(matriz);
 
-
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
-const mat = [1, 0, 1, 0, 0];
+vecinos(matriz)
 
 const drawMatrix = (m) => {
-    
     for (let i = 0; i < m.length; i++) {
-        const y = i * 15;
-
-        ctx.rect( y, 15, 15, 15) ;
+        for (let j = 0; j < m[i].length; j++) {
+            const y = i * 15;
+            const x = j * 15;
     
-        if (m[i] === 0) {
-            ctx.Style = "rgb(0,0,0)";
-           
-        } else {
-            ctx.Style = "rgb(255,255,255)";
-            
-        };
-
-        ctx.fill();
-
-        
+            if (m[i][j] === 0) {
+                ctx.fillStyle = "rgb(255,255,255)";
+            } else {
+                ctx.fillStyle = "rgb(0,0,0)";
+            }
+            ctx.fillRect(y, x, 15, 15);
+            ctx.lineWidth = 1.5;
+            ctx.strokeStyle = "rgb(0,0,0)";
+            ctx.strokeRect(y, x, 15, 15);
+        }
     }
 }
+
+drawMatrix(matriz);
+
 drawMatrix(mat);
